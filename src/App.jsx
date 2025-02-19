680527cc6db14c104f8f7abd6f49818bfbc6b72d
Nkugavarathan <kugavarathan28@gmail.com>
2025-02-19 20:05:05 +0530

    navbar styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react"
+import { Routes, Route, Link } from "react-router-dom"
 import Navbar from "./pages/Navbar"
+import Home from "./pages/Home"
+import Contact from "./pages/Contact"
+import About from "./pages/About"
+import Products from "./pages/Products"
+
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
     </div>
   )
 }
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,15 +1,27 @@
 import React from "react"
-
+import { Link } from "react-router-dom"
 export default function Navbar() {
   return (
     <div className="navbar">
       <div className="logo">Varathan</div>
       <div className="navlist">
-        <a href="">Home</a>
-        <a href="">Products</a>
-        <a href="">About</a>
-        <a href="">Contact</a>
-        <button>Login</button>
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/products" className="nav-link">
+          Products
+        </Link>
+        <Link to="/about" className="nav-link">
+          About
+        </Link>
+        <Link to="/contact" className="nav-link">
+          Contact
+        </Link>
+        <button>
+          <Link to="/" className="nav-link">
+            Login
+          </Link>
+        </button>
       </div>
     </div>
   )
